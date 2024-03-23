e53383398dad8571902fbf263a5f3bb81094aa04
yoyounik <sinhanikhil909@gmail.com>
2024-03-23 14:17:07 +0530

    Lombok added

--- src/main/java/com/edgest/myfirstprojectfirse/entity/JournalEntry.java
+++ src/main/java/com/edgest/myfirstprojectfirse/entity/JournalEntry.java
@@ -1,37 +1,17 @@
 package com.edgest.myfirstprojectfirse.entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="journal_entries")
+@Data
 public class JournalEntry {
     @Id
     private ObjectId id;
     private String title;
     private String content;
 
-    public ObjectId getId() {
-        return id;
-    }
 
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
